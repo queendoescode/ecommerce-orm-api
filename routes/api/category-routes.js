2f6f227b3299f68c967af5399de3715f9d45560d
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-25 15:59:25 -0400

    added GET and POST routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,43 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const data = await Category.findAll({include: Product});
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in fetching data"});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categoryId = req.params.id;
+
+  try {
+    const data = await Category.findByPk(categoryId, {include: Product});
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in fetching data"});
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/',  async (req, res) => {
   // create a new category
+  const categoryName = req.body.category_name;
+
+  try {
+    const data = await Category.create({category_name: categoryName});
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in creating new data"});
+  }
 });
 
 router.put('/:id', (req, res) => {
