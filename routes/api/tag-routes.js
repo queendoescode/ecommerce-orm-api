e80c71f255bd6ca15bdb616f22483440187f876f
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-26 20:04:12 -0400

    added post, put and delete routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,16 +29,59 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/',async (req, res) => {
   // create a new tag
+  const tagName = req.body.tag_name;
+
+  try {
+    const data = await Tag.create({tag_name: tagName});
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in creating new data"});
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const tagId= req.params.id;
+  const tagName = req.body.tag_name;
+
+  try {
+    const data = await Tag.update(
+      {
+        tag_name: tagName
+      },
+      { 
+        where: {
+          id: tagId
+        }
+      });
+    const updatedRow = await Tag.findByPk(tagId);
+    res.json(updatedRow);
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in updating data"});
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const tagId = req.params.id;
+
+  try {
+    await Tag.destroy(
+      { 
+        where: {
+          id: tagId
+        }
+      }
+    );
+    res.json({});
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in deleting data"});
+  }
 });
 
 module.exports = router;
