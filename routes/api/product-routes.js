cf675ae3acc8ad8910f544e10111a6c59f077f8e
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-27 15:39:07 -0400

    added routes for get one product and get all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+
+  try {
+    const data = await    Product.findAll({include: [Category, Tag]});
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in fetching data"});
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const productId = req.params.id;
+
+  try {
+    const data = await  Product.findByPk(productId, {include: [Category, Tag]});
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json({error: "An error occurred in fetching data"});
+  }
 });
 
 // create new product
